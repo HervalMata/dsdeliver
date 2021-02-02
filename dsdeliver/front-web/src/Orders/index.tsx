6a271c5b74a1bb85a217876aab85e6f0fa425eab
HervalMata <hervalmataamparo@gmail.com>
2021-02-02 02:05:43 -0300

    Search Orders Location

--- dsdeliver/front-web/src/Orders/index.tsx
+++ dsdeliver/front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import { Product, OrderLocationdata } from  './types';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
 import './styles.css';
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+
+  useEffect(() => {
+    fetchProducts () 
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
+      <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
     </div>
   )
 }
